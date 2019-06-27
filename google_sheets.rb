588abdbafbf04ea6ff7cfea07b8ccbb0dc746ad2
Karen Majewicz <2367677+karenmajewicz@users.noreply.github.com>
2019-06-27 17:10:09 -0500

    Add files via upload

---
+++ google_sheets.rb
@@ -0,0 +1,98 @@
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org'
+  gem 'json'
+  gem 'iiif-presentation'
+end
+
+require 'csv'
+require 'open-uri'
+require 'iiif/presentation'
+
+# Open CSV File
+csv_file = CSV.read("lincoln.csv", headers:true, header_converters: :symbol)
+
+parent_record = {}
+
+csv_file.each do |row|
+  parent_record[:dc_title] = row[:dc_title]
+  parent_record[:metadata] = []
+  parent_record[:metadata] << {
+    label: "Description",
+    value: row[:dc_description]
+  }
+
+  parent_record[:metadata] << {
+    label: "Creator",
+    value: row[:dc_creator]
+  }
+
+  parent_record[:metadata] << {
+    label: "Keywords",
+    value: row[:keywords]
+  }
+  break
+end
+
+puts parent_record.inspect
+
+seed = {
+  "@context" => "http://iiif.io/api/presentation/2/context.json",
+  "@id" => 'http://foo.ngrok.io/iiif_manifest.json',
+  "@type" => "sc:Manifest",
+  "label" => parent_record[:dc_title],
+  "metadata" => parent_record[:metadata],
+  "seeAlso" => []
+}
+
+# Any options you add are added to the object
+manifest = IIIF::Presentation::Manifest.new(seed)
+
+puts manifest.inspect
+
+manifest.sequences <<
+  {
+    "@type" => "sc:Sequence",
+    "canvases" => []
+  }
+
+CSV.foreach("lincoln.csv", {headers:true, header_converters: :symbol}).with_index do |row, i|
+  next if i == 0
+
+  # Canvas for each page
+  canvas = IIIF::Presentation::Canvas.new()
+
+  canvas["@type"] = "sc:Canvas"
+  canvas['@id'] = row[:gbl_web_map_service]
+  canvas["label"] = row[:dc_title]
+  canvas["height"] = 1500
+  canvas["width"] = 1500
+
+  service_url = row[:gbl_web_map_service]
+  service_data = JSON.parse(open(service_url).read)
+
+  image = {
+    "@type": "oa:Annotation",
+    "motivation": "sc:painting",
+    "on": row[:gbl_web_map_service],
+    "resource": {
+      "@id": row[:gbl_web_map_service],
+      "@type": "dctypes:Image",
+      "format": "image/jpeg",
+      "height": 1500,
+      "width": 1500,
+      "service": service_data
+    }
+  }
+
+  canvas["images"] = []
+  canvas["images"] << image
+
+  manifest.sequences[0]["canvases"] << canvas
+end
+
+# Write manifest file
+File.open("iiif_manifest.json","w") do |f|
+  f.write(manifest.to_json(pretty: true))
+end
